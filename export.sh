a073f7a69e309690d1c9cfac78be3ad5483a9199
latyr-embdio <latyr.fall@embd-io.com>
2025-04-08 03:55:43 -0400

    Fix export script

--- export.sh
+++ export.sh
@@ -3,7 +3,7 @@
 # Gstreamer plugin for Kinesis Video Streams
 KVS_SDK_PATH="amazon-kinesis-video-streams-producer-sdk-cpp"
 KVS_BUILD_PATH="`pwd`/${KVS_SDK_PATH}/build"
-export GST_PLUGIN_PATH=KVS_BUILD_PATH
+export GST_PLUGIN_PATH=$KVS_BUILD_PATH
 
 # AWS KVS credentials
 export AWS_ACCESS_KEY_ID="xxxxxxxxxxxxxxxxxxxx"
