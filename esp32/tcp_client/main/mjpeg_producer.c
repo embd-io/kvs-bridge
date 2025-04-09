b40b122c5fb0c97eebbe9b9f24dce34cf5e1a385
latyr-embdio <latyr.fall@embd-io.com>
2025-04-09 14:26:24 -0400

    Update ESP32 based mjpeg producer  implementation

--- esp32/tcp_client/main/mjpeg_producer.c
+++ esp32/tcp_client/main/mjpeg_producer.c
@@ -8,22 +8,17 @@
 #include "esp_netif.h"
 #include "esp_log.h"
 
+static const char *TAG = "mjpeg_producer";
+
 // Server config
 #define HOST_IP_ADDR CONFIG_EXAMPLE_IPV4_ADDR
 #define PORT CONFIG_EXAMPLE_PORT
 
-static const char *TAG = "kbridge_client";
+// Static MJPEG image data
+extern const uint8_t *frames_jpg_start[];
+extern const uint8_t *frames_jpg_end[];
 
-extern const uint8_t frame_0001_jpg_start[] asm("_binary_frame_0001_jpg_start");
-extern const uint8_t frame_0001_jpg_end[] asm("_binary_frame_0001_jpg_end");
-
-const uint8_t *frames_jpg_start[] = {
-    frame_0001_jpg_start,
-};
-
-const uint8_t *frames_jpg_end[] = {
-    frame_0001_jpg_end,
-};
+const float frame_rate_ms = 1000 / 30; // 30 fps
 
 void kbridge_client(void)
 {
@@ -32,24 +27,15 @@ void kbridge_client(void)
     int addr_family = 0;
     int ip_protocol = 0;
 
-    uint32_t uDataLen = frames_jpg_end[0] - frames_jpg_start[0] + 1;
-    char* pData = (char *)malloc(uDataLen);
-    if (pData == NULL) {
-        ESP_LOGE(TAG, "Memory allocation failed");
-        while (1) {
-            vTaskDelay(1000 / portTICK_PERIOD_MS);
-        }
-    }
-    memcpy(pData, frames_jpg_start[0], uDataLen);
-
     while (1) {
+        
+        // Socket configuration
         struct sockaddr_in dest_addr;
         inet_pton(AF_INET, host_ip, &dest_addr.sin_addr);
         dest_addr.sin_family = AF_INET;
         dest_addr.sin_port = htons(PORT);
         addr_family = AF_INET;
         ip_protocol = IPPROTO_IP;
-
         int sock =  socket(addr_family, SOCK_STREAM, ip_protocol);
         if (sock < 0) {
             ESP_LOGE(TAG, "Unable to create socket: errno %d", errno);
@@ -57,6 +43,7 @@ void kbridge_client(void)
         }
         ESP_LOGI(TAG, "Socket created, connecting to %s:%d", host_ip, PORT);
 
+        // Connect to server
         int err = connect(sock, (struct sockaddr *)&dest_addr, sizeof(dest_addr));
         if (err != 0) {
             ESP_LOGE(TAG, "Socket unable to connect: errno %d", errno);
@@ -64,7 +51,24 @@ void kbridge_client(void)
         }
         ESP_LOGI(TAG, "Successfully connected");
 
+        int nb_images = sizeof((uint8_t*)frames_jpg_start) / sizeof(frames_jpg_start[0]);
+        int image_index = 0;
+
         while (1) {
+
+            int idx = image_index % nb_images;
+            uint32_t uDataLen = frames_jpg_end[idx] - frames_jpg_start[idx] + 1;
+            char* pData = (char *)malloc(uDataLen);
+            if (pData == NULL) {
+                ESP_LOGE(TAG, "Memory allocation failed for image %d", image_index);
+                while (1) {
+                    vTaskDelay(1000 / portTICK_PERIOD_MS);
+                }
+            }
+            memcpy(pData, frames_jpg_start[idx], uDataLen);
+            ESP_LOGI(TAG, "Image %d size: %ld", (image_index % nb_images) / nb_images, uDataLen);
+            image_index++;
+
             // Send image size
             ESP_LOGI(TAG, "Sending image size: %ld", uDataLen);
             uint32_t uDataLen_be = htonl(uDataLen);
@@ -80,23 +84,7 @@ void kbridge_client(void)
             }
             ESP_LOGI(TAG, "Image data sent successfully");
 
-
-            vTaskDelay(30 / portTICK_PERIOD_MS);
-#if 0
-            int len = recv(sock, rx_buffer, sizeof(rx_buffer) - 1, 0);
-            // Error occurred during receiving
-            if (len < 0) {
-                ESP_LOGE(TAG, "recv failed: errno %d", errno);
-                break;
-            }
-            // Data received
-            else {
-                rx_buffer[len] = 0; // Null-terminate whatever we received and treat like a string
-                ESP_LOGI(TAG, "Received %d bytes from %s:", len, host_ip);
-                ESP_LOGI(TAG, "%s", rx_buffer);
-            }
-#endif 
-
+            vTaskDelay(frame_rate_ms / portTICK_PERIOD_MS);
         }
 
         if (sock != -1) {
---
+++ esp32/tcp_client/main/static_frames.c
@@ -0,0 +1,54 @@
+#include <stdint.h>
+
+extern const uint8_t frame_0001_jpg_start[] asm("_binary_frame_0001_jpg_start");
+extern const uint8_t frame_0010_jpg_start[] asm("_binary_frame_0010_jpg_start");
+extern const uint8_t frame_0020_jpg_start[] asm("_binary_frame_0020_jpg_start");
+extern const uint8_t frame_0030_jpg_start[] asm("_binary_frame_0030_jpg_start");
+extern const uint8_t frame_0040_jpg_start[] asm("_binary_frame_0040_jpg_start");
+extern const uint8_t frame_0050_jpg_start[] asm("_binary_frame_0050_jpg_start");
+extern const uint8_t frame_0060_jpg_start[] asm("_binary_frame_0060_jpg_start");
+extern const uint8_t frame_0070_jpg_start[] asm("_binary_frame_0070_jpg_start");
+extern const uint8_t frame_0080_jpg_start[] asm("_binary_frame_0080_jpg_start");
+extern const uint8_t frame_0090_jpg_start[] asm("_binary_frame_0090_jpg_start");
+extern const uint8_t frame_0100_jpg_start[] asm("_binary_frame_0100_jpg_start");
+
+extern const uint8_t frame_0001_jpg_end[] asm("_binary_frame_0001_jpg_end");
+extern const uint8_t frame_0010_jpg_end[] asm("_binary_frame_0010_jpg_end");
+extern const uint8_t frame_0020_jpg_end[] asm("_binary_frame_0020_jpg_end");
+extern const uint8_t frame_0030_jpg_end[] asm("_binary_frame_0030_jpg_end");
+extern const uint8_t frame_0040_jpg_end[] asm("_binary_frame_0040_jpg_end");
+extern const uint8_t frame_0050_jpg_end[] asm("_binary_frame_0050_jpg_end");
+extern const uint8_t frame_0060_jpg_end[] asm("_binary_frame_0060_jpg_end");
+extern const uint8_t frame_0070_jpg_end[] asm("_binary_frame_0070_jpg_end");
+extern const uint8_t frame_0080_jpg_end[] asm("_binary_frame_0080_jpg_end");
+extern const uint8_t frame_0090_jpg_end[] asm("_binary_frame_0090_jpg_end");
+extern const uint8_t frame_0100_jpg_end[] asm("_binary_frame_0100_jpg_end");
+
+
+const uint8_t *frames_jpg_start[] = {
+    frame_0001_jpg_start,
+    frame_0010_jpg_start,
+    frame_0020_jpg_start,
+    frame_0030_jpg_start,
+    frame_0040_jpg_start,
+    frame_0050_jpg_start,
+    frame_0060_jpg_start,
+    frame_0070_jpg_start,
+    frame_0080_jpg_start,
+    frame_0090_jpg_start,
+    frame_0100_jpg_start,
+};
+
+const uint8_t *frames_jpg_end[] = {
+    frame_0001_jpg_end,
+    frame_0010_jpg_end,
+    frame_0020_jpg_end,
+    frame_0030_jpg_end,
+    frame_0040_jpg_end,
+    frame_0050_jpg_end,
+    frame_0060_jpg_end,
+    frame_0070_jpg_end,
+    frame_0080_jpg_end,
+    frame_0090_jpg_end,
+    frame_0100_jpg_end,
+};
