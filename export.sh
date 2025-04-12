1f7500a28bbfeac1b4b7505a01e4e8f7731f3beb
latyr-embdio <latyr.fall@embd-io.com>
2025-04-12 07:18:23 -0400

    Add default region to exported AWS prperties

--- export.sh
+++ export.sh
@@ -6,5 +6,6 @@ KVS_BUILD_PATH="`pwd`/${KVS_SDK_PATH}/build"
 export GST_PLUGIN_PATH=$KVS_BUILD_PATH
 
 # AWS KVS credentials
+export AWS_DEFAULT_REGION="xx-yyyy-N"
 export AWS_ACCESS_KEY_ID="xxxxxxxxxxxxxxxxxxxx"
 export AWS_SECRET_ACCESS_KEY="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
